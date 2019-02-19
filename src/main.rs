530a25babf752fea519fcfbd402fb5f375d0a05d
maximforever <flufci@gmail.com>
2019-02-19 16:25:59 -0500

    fix last word spacing bug throughout

--- src/main.rs
+++ src/main.rs
@@ -11,29 +11,48 @@ fn main() {
 
     let mut lines = Vec::new();
 
-    // TODO: what if multiple whitespace
     // TODO: what if words > 40
     let words = args.text.split(" ");
     let mut substring = String::new();
     let mut max_line_length = 0;
 
-    for word in words {
-        // TODO: don't push space for last word
-        if substring.len() + word.len() > 39 {
-            //let spaces = iter::repeat(" ").take(40 - substring.len()).collect::<String>();
-            //substring.push_str(&spaces);
+    for (i, word) in words.enumerate() {
+
+        if word.len() > 40 {
+
+            for (i, c) in word.chars().enumerate(){
+                println!("{} {}", i, c);
+            }
+
+
+            /* 
+                1. iterate through word by character, keeping track of the substring length so far
+                2. if we're on char 39, add a hyphen and start a new substring from the following letter
+            */
+           
+
+           
+
+        } else if substring.len() + word.len() > 40 {
+
             max_line_length = cmp::max(max_line_length, substring.len());
+            
+            // starts a new line 
             lines.push(substring);
             substring = String::from(word);
-            substring.push_str(" ");
+
         } else {
+
+            // this is triggered for the first word
+            if i != 0{
+                substring.push_str(" ");
+            }
+            
             substring.push_str(word);
-            substring.push_str(" ");
         }
     }
+
     if substring.len() > 0 {
-        //let spaces = iter::repeat(" ").take(40 - substring.len()).collect::<String>();
-        //substring.push_str(&spaces);
         max_line_length = cmp::max(max_line_length, substring.len());
         lines.push(substring);
     }
@@ -44,15 +63,6 @@ fn main() {
         line
     }).collect::<Vec<_>>();
 
-
-    //for mut line in lines {
-    //    if line.len() < max_line_length {
-    //        let spaces = iter::repeat(" ").take(max_line_length - line.len()).collect::<String>();
-    //        let padded_line
-    //        let padded_line = line.push_str(&spaces);
-    //        padded_lines.push(padded_line);
-    //    }
-    //}
 
     let horizontal_border = iter::repeat("-").take(max_line_length + 2).collect::<String>();
         
